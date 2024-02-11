ea1c9a811122f776ff47dea0971a275a4ae98599
Bantyque <159322383+Bantyque@users.noreply.github.com>
2024-02-11 17:56:40 +0200

    Add files via upload

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,5 @@
+rofi \
+  -show p \
+  -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
+  -font "Iosevka Nerd Font 16" \
+  -theme-str 'window {width: 8em;} listview {lines: 6;}'
